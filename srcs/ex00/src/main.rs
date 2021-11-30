dc75c54eec731c7e87560f3195e620d77423c162
xaviave <hellxaviave@gmail.com>
2021-11-30 17:41:04 +0100

    adder done

---
+++ srcs/ex00/src/main.rs
@@ -0,0 +1,76 @@
+fn adder(a: u32, b: u32) -> u32 {
+	// https://iq.opengenus.org/addition-using-bitwise-operations/
+	if b == 0
+	{
+		return a;
+	}
+	return adder(a^b, (a&b) << 1);	
+}
+
+#[cfg(test)]
+mod tests {
+	// https://doc.rust-lang.org/rust-by-example/testing/unit_testing.html
+	use super::*;
+
+	#[test]
+	fn test_zero_sum_1() {
+		assert_eq!(adder(0, 0), 0);
+	}
+
+	#[test]
+	fn test_zero_sum_2() {
+		assert_eq!(adder(0, 10), 10);
+	}
+
+	#[test]
+	fn test_zero_sum_3() {
+		assert_eq!(adder(200, 0), 200);
+	}
+
+	#[test]
+	fn test_sum_1() {
+		assert_eq!(adder(1, 1), 2);
+	}
+	
+	#[test]
+	fn test_sum_2() {
+		assert_eq!(adder(1456, 1000), 2456);
+	}
+	
+	#[test]
+	fn test_overflow_1() {
+		assert_eq!(adder(4_294_967_292, 3), 4_294_967_295);
+	}
+
+	#[test]
+	fn test_overflow_2() {
+		assert_eq!(adder(4_294_967_292, 3), 4_294_967_295);
+	}
+
+	#[test]
+	fn test_overflow_3() {
+		assert_eq!(adder(4_294_967_295, 2), 1);
+	}
+
+	#[test]
+    #[should_panic]
+	fn test_bad_sum_1() {
+		assert_eq!(adder(1, 0), 0);
+	}
+
+	#[test]
+    #[should_panic]
+	fn test_bad_sum_2() {
+		assert_eq!(adder(1, 11110), 2470);
+	}
+
+	// #[test]
+	// #[should_panic]
+	// fn test_bad_sign() {
+	// 	assert_eq!(adder(-1110, 1110), 0);
+	// }
+}
+
+fn main() {
+	println!("add 1 + 2 = {}", adder(1, 2));
+}
---
+++ srcs/ex00/target/debug/ex00.d
@@ -0,0 +1,1 @@
+/home/gmx/ready_set_bool/srcs/ex00/target/debug/ex00: /home/gmx/ready_set_bool/srcs/ex00/src/main.rs
